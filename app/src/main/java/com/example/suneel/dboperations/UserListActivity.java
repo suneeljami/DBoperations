0ded328e5a2aacd150f22b4462aea1dc0bbd74f7
suneel <suneelkumarjami453@mail.com>
2018-02-26 22:06:55 +0530

    adding recycler view

--- app/src/main/java/com/example/suneel/dboperations/UserListActivity.java
+++ app/src/main/java/com/example/suneel/dboperations/UserListActivity.java
@@ -5,12 +5,18 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.animation.OvershootInterpolator;
 
-public class UserListActivity extends AppCompatActivity {
+import com.example.suneel.dboperations.adapter.MyAdapter;
 
+public class UserListActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +39,21 @@ public class UserListActivity extends AppCompatActivity {
                 setDuration(500).
                 setInterpolator(interpolator).
                 start();
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        String[] myDataset={"suneel","nagendra","dilli"};
+        mAdapter = new MyAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
+
     }
 
 }
